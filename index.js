b6d9e965b218bd4aaedc3cc063cff8e6b84b5bdb
Zachary <zacharylampi96@gmail.com>
2022-06-08 17:19:23 -0400

    commit tree f13df6973b6dde7a40ab99fd68c5c76352241874

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// code your solution here
+// // code your solution here
+const record = [
+    { year: "2018", result: "N/A"},
+    { year: "2017", result: "N/A"},
+    { year: "2016", result: "N/A"},
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    { year: "2012", result: "N/A"},
+    { year: "2011", result: "N/A"},
+    { year: "2010", result: "N/A"},
+    { year: "2009", result: "N/A"},
+    { year: "2008", result: "N/A"},]
+
+// let foundItem = record.find(superbowlWin)
+//         console.log(foundItem)
+
+function superbowlWin(item) {
+    let foundItem = item.find(nothing)
+    console.log(foundItem)
+    if (!foundItem){
+        return undefined
+    }
+return foundItem.year
+}
+
+function nothing(item) {    
+    if( item.result === "W")
+    {return true}}
+foundItem.year
